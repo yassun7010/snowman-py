0b070dff745d105c24e791aa16b4cae160deaacd
yassun7010 <yassun7010@outlook.com>
2024-05-03 09:08:32 +0900

    docs: add comment.

--- src/snowman/protocol/cursor.py
+++ src/snowman/protocol/cursor.py
@@ -2,6 +2,14 @@ from typing import Any, Protocol
 
 
 class Cursor(Protocol):
+    """
+    Cursor protocol.
+
+    This implements the minimum required interface for snowman.
+
+    Reference: [PEP 249 – Python Database API Specification v2.0](https://peps.python.org/pep-0249/#id20)
+    """
+
     def execute(self, *args, **kwargs) -> Any: ...
 
     def executemany(self, *args, **kwargs) -> Any: ...
