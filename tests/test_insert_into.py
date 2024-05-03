6676c0cf8bba21cc3e07cf19d858a39cfe4b63f0
yassun7010 <yassun7010@outlook.com>
2024-05-03 09:21:43 +0900

    chore: fix test case.

--- tests/test_insert_into.py
+++ tests/test_insert_into.py
@@ -1,8 +1,8 @@
+import snowman
 from conftest import User
 from snowman.protocol.cursor import Cursor
-from snowman.query import insert_into
 
 
 class TestInsertInto:
     def test_insert_into_statement(self, user: User, cursor: Cursor):
-        insert_into(User).values(user, user).execute(cursor)
+        snowman.query.insert_into(User).values(user, user).execute(cursor)
--- tests/test_update.py
+++ tests/test_update.py
@@ -1,8 +1,8 @@
+import snowman
 from conftest import User
 from snowman.protocol.cursor import Cursor
-from snowman.query import update
 
 
 class TestUpdateStatement:
     def test_update_statement(self, user: User, cursor: Cursor):
-        update(User).set(user).execute(cursor)
+        snowman.query.update(User).set(user).execute(cursor)
