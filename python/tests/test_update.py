4f1f72a69e93519f630d38ded35cd265ab424169
yassun7010 <yassun7010@outlook.com>
2024-05-19 22:49:17 +0900

    chore: add test case.

--- python/tests/test_update.py
+++ python/tests/test_update.py
@@ -1,4 +1,5 @@
 import textwrap
+
 import snowq
 from conftest import User
 from snowflake.connector.cursor import SnowflakeCursor
@@ -10,7 +11,25 @@ class TestUpdateQuery:
             mock_snowflake_cursor
         )
 
-    def test_update_query_build(self, user: User):
+    def test_update_query_dict_build(
+        self, user: User, mock_snowflake_cursor: SnowflakeCursor
+    ):
+        query, params = snowq.query.update(User).set({"name": "taro"}).build()
+
+        assert (
+            query
+            == textwrap.dedent(
+                """
+                UPDATE
+                    database.public.users
+                SET
+                    name = %(name)s
+                """
+            ).strip()
+        )
+        assert params == {"name": "taro"}
+
+    def test_update_query_pydantic_build(self, user: User):
         query, params = snowq.query.update(User).set(user).build()
 
         assert (
