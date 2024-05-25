2aba6a644e2a30920b53fbf27e86b603dd77254f
yassun7010 <yassun7010@outlook.com>
2024-05-25 22:31:39 +0900

    refactor: add fn.

--- crates/snowq/src/command/schema_sync.rs
+++ crates/snowq/src/command/schema_sync.rs
@@ -2,6 +2,7 @@ use crate::config::{get_pydantic_options, get_schema_output_dirpath};
 use clap::Args;
 use convert_case::{Case, Casing};
 use itertools::Itertools;
+use snowq_connector::query::DatabaseSchema;
 use std::iter::Iterator;
 use tokio::io::AsyncWriteExt;
 
@@ -93,27 +94,7 @@ pub async fn run_schema_sync_command(
             .into_group_map_by(|x| x.database_name.clone())
             .into_iter()
             .map(|(database_name, schemas)| async move {
-                let schema_output_dirpath = &schema_output_dirpath;
-                let database_dir = schema_output_dirpath.join(database_name.to_case(Case::Snake));
-                let schema_names = schemas
-                    .iter()
-                    .map(|schema| schema.schema_name.to_case(Case::Snake))
-                    .collect::<Vec<_>>();
-
-                tokio::fs::File::create(database_dir.join("__init__.py"))
-                    .await?
-                    .write_all(
-                        snowq_generator::generate_modlue_init_py(
-                            &schema_names
-                                .iter()
-                                .map(AsRef::as_ref)
-                                .collect::<Vec<&str>>(),
-                        )
-                        .as_bytes(),
-                    )
-                    .await?;
-
-                Ok::<(), Box<dyn std::error::Error>>(())
+                write_schema_init_py(schema_output_dirpath, &database_name, &schemas).await
             }),
     )
     .await?;
@@ -138,4 +119,32 @@ pub async fn run_schema_sync_command(
         .await?;
 
     Ok(())
+}
+
+async fn write_schema_init_py(
+    schema_output_dirpath: &std::path::Path,
+    database_name: &str,
+    schemas: &[&DatabaseSchema],
+) -> Result<(), Box<dyn std::error::Error>> {
+    let schema_output_dirpath = &schema_output_dirpath;
+    let database_dir = schema_output_dirpath.join(database_name.to_case(Case::Snake));
+    let schema_names = schemas
+        .iter()
+        .map(|schema| schema.schema_name.to_case(Case::Snake))
+        .collect::<Vec<_>>();
+
+    tokio::fs::File::create(database_dir.join("__init__.py"))
+        .await?
+        .write_all(
+            snowq_generator::generate_modlue_init_py(
+                &schema_names
+                    .iter()
+                    .map(AsRef::as_ref)
+                    .collect::<Vec<&str>>(),
+            )
+            .as_bytes(),
+        )
+        .await?;
+
+    Ok::<(), Box<dyn std::error::Error>>(())
 }
