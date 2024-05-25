316a1615ad3e7c1f585f0b9bcfe2c4b8c3225757
yassun7010 <yassun7010@outlook.com>
2024-05-25 17:03:08 +0900

    chore: sample.

--- examples/snowq_sample/src/snowq_sample/__init__.py
+++ examples/snowq_sample/src/snowq_sample/__init__.py
@@ -1,2 +1,7 @@
-def hello() -> str:
-    return "Hello from snowq-sample!"
+import snowq
+
+from snowq_sample.schema import cdp
+
+snowq.query.insert.into(cdp.datalake.AdsDataHubData).values(
+    cdp.datalake.AdsDataHubData.model_validate({})
+)
