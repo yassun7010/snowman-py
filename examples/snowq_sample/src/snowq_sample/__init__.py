a6af37fb4b3a2f1e06b0d8315d164054b2966d39
yassun7010 <yassun7010@outlook.com>
2024-05-30 01:22:43 +0900

    chore: change sample.

--- examples/snowq_sample/src/snowq_sample/__init__.py
+++ examples/snowq_sample/src/snowq_sample/__init__.py
@@ -1,11 +1,17 @@
-import snowq
+from snowq.query import insert, update
 
 from snowq_sample.schema import cdp
 
-snowq.query.insert.into(cdp.datalake.AdsDataHubData).values(
-    cdp.datalake.AdsDataHubData.model_validate({})
+insert.into(
+    cdp.datalake.AdsDataHubData,
+).values(
+    cdp.datalake.AdsDataHubData.model_validate({}),
 )
 
-snowq.query.update(cdp.datalake.AdsDataHubData).set({"campaign_id": "1234"}).where(
-    "campaign_id = 1"
+update(
+    cdp.datalake.AdsDataHubData,
+).set(
+    {"campaign_id": "1234"},
+).where(
+    "campaign_id = 1",
 )
