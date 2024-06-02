a65e448a0142c80a9946436a9851872d0d9b463d
yassun7010 <yassun7010@outlook.com>
2024-06-02 23:40:26 +0900

    fix: arg.

--- crates/snowq/src/command/config/create.rs
+++ crates/snowq/src/command/config/create.rs
@@ -1,11 +1,16 @@
 #[derive(clap::Args)]
 pub struct Args {
-    #[clap(long)]
+    /// The path to the snowq configuration file.
+    #[arg(long, default_value = "snowq.toml")]
+    pub file: std::path::PathBuf,
+
+    /// Overwrite the existing configuration file.
+    #[arg(long)]
     pub overwrite: bool,
 }
 
 pub fn run(args: Args) -> Result<(), anyhow::Error> {
-    let config_filepath = std::path::PathBuf::from("snowq.toml");
+    let config_filepath = args.file;
     if config_filepath.exists() && !args.overwrite {
         return Err(anyhow::anyhow!(format!(
             "{config_filepath:?} already exists."
