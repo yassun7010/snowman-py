15fde061921268705935e5855c9e7fceb70067f9
yassun7010 <yassun7010@outlook.com>
2024-06-04 22:14:43 +0900

    fix: ci.

--- python/src/snowman/__init__.py
+++ python/src/snowman/__init__.py
@@ -6,4 +6,4 @@ from . import query as query
 from .relation.table import Table as Table
 from .relation.table import table as table
 
-__version__ = importlib.metadata.version("snowman")
+__version__ = importlib.metadata.version("snowman-py")
