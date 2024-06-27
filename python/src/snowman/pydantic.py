1b162df57279a34f4977bad274d664c6d3c19071
yassun7010 <yassun7010@outlook.com>
2024-06-27 22:17:16 +0900

    docs: move to docstring.

--- python/src/snowman/pydantic.py
+++ python/src/snowman/pydantic.py
@@ -3,8 +3,10 @@ from typing import Any, Callable
 from pydantic import Field
 
 
-# The wrapper for cohabitation of Annotated and default_factory.
-#
-# See: https://github.com/pydantic/pydantic/issues/9769
 def DefaultFactory(default_factory: Callable[[], Any]) -> Any:
+    """
+    The wrapper for cohabitation of Annotated and default_factory.
+
+    See: https://github.com/pydantic/pydantic/issues/9769
+    """
     return Field(default_factory=default_factory)
