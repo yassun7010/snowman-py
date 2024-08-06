82963da2968cead68e640cbcaf13423f94ead1ae
yassun7010 <yassun7010@outlook.com>
2024-08-07 01:31:29 +0900

    wip: code.

--- docs/data/delete_query.py
+++ docs/data/delete_query.py
@@ -1,7 +1,6 @@
 from snowman.query import delete
+from your.database.schema import User
 from your.snowflake import snowflake_conn
-
-from docs.data.your.database.schema import User
 
 with snowflake_conn.cursor() as cursor:
     delete.from_(User).where(
