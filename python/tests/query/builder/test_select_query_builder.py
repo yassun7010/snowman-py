f6671d644bbf641e6129dd72978bd5e66b0b12e5
yassun7010 <yassun7010@outlook.com>
2024-09-11 23:50:19 +0900

    test: add test case.

--- python/tests/query/builder/test_select_query_builder.py
+++ python/tests/query/builder/test_select_query_builder.py
@@ -4,11 +4,12 @@ import pytest
 import snowman.query
 from conftest import REAL_TEST_IS_DESABLED, User
 from snowflake.connector.connection import SnowflakeConnection
+from snowman.query import select
 
 
 class TestSelectQueryBuilder:
     def test_select_all_build(self):
-        query, params = snowman.query.select().from_(User).build()
+        query, params = select().from_(User).build()
         assert (
             query
             == textwrap.dedent(
@@ -22,6 +23,51 @@ class TestSelectQueryBuilder:
         )
         assert params == ()
 
+    # def test_select_specific_columns(self):
+    #     query, params = select({"id": User.id, "name": "Alice"}).from_(User).build()
+    #     assert query == "SELECT id, name FROM test_table"
+
+    def test_select_with_where_clause_build(self):
+        query, params = select().from_(User).where(lambda c: c.self.id > 18).build()
+        assert (
+            query
+            == textwrap.dedent(
+                """
+                SELECT
+                    *
+                FROM
+                    database.schema.users
+                WHERE
+                    id > %s
+                """
+            ).strip()
+        )
+        assert params == (18,)
+
+    # def test_select_with_order_by_build(self):
+    #     query, params = select().from_(User).order_by(lambda c: c.self.name).build()
+    #     assert query == "SELECT * FROM test_table ORDER BY name ASC"
+    #     assert params == ()
+
+    # def test_select_with_limit_build(self):
+    #     query, params = select().from_(User).limit(10).build()
+    #     assert query == "SELECT * FROM test_table LIMIT 10"
+    #     assert params == ()
+
+    # def test_select_with_multiple_conditions(self, user: User):
+    #     query = (
+    #         select()
+    #         .from_(User)
+    #         .where(lambda c: (c.self.id > 18) & (c.self.name.like("A%")))
+    #         .order_by(lambda c: c.self.age.desc)
+    #         .limit(5)
+    #         .build()
+    #     )
+    #     assert (
+    #         query
+    #         == "SELECT name, age FROM test_table WHERE age > 18 AND name LIKE 'A%' ORDER BY age DESC LIMIT 5"
+    #     )
+
     @pytest.mark.skipif(**REAL_TEST_IS_DESABLED)
     def test_select_all_execute_fetchall(
         self,
@@ -33,7 +79,7 @@ class TestSelectQueryBuilder:
         with snowflake_connection.cursor() as cursor:
             snowman.query.truncate(RealUser).execute(cursor)
             snowman.query.insert.into(RealUser).values(real_user).execute(cursor)
-            users = snowman.query.select().from_(RealUser).execute(cursor).fetchall()
+            users = select().from_(RealUser).execute(cursor).fetchall()
 
             assert users == [real_user]
 
@@ -48,7 +94,7 @@ class TestSelectQueryBuilder:
         with snowflake_connection.cursor() as cursor:
             snowman.query.truncate(RealUser).execute(cursor)
             snowman.query.insert.into(RealUser).values(real_user).execute(cursor)
-            users = snowman.query.select().from_(RealUser).execute(cursor).fetchmany()
+            users = select().from_(RealUser).execute(cursor).fetchmany()
 
             assert users == [real_user]
 
@@ -63,7 +109,7 @@ class TestSelectQueryBuilder:
         with snowflake_connection.cursor() as cursor:
             snowman.query.truncate(RealUser).execute(cursor)
             snowman.query.insert.into(RealUser).values(real_user).execute(cursor)
-            user = snowman.query.select().from_(RealUser).execute(cursor).fetchone()
+            user = select().from_(RealUser).execute(cursor).fetchone()
 
             assert user == real_user
 
@@ -77,41 +123,6 @@ class TestSelectQueryBuilder:
 
         with snowflake_connection.cursor() as cursor:
             snowman.query.truncate(RealUser).execute(cursor)
-            user = snowman.query.select().from_(RealUser).execute(cursor).fetchone()
+            user = select().from_(RealUser).execute(cursor).fetchone()
 
             assert user is None
-
-
-# def test_select_specific_columns():
-#     query = select().from_(TestTable).columns(TestTable.id, TestTable.name).build()
-#     assert query == "SELECT id, name FROM test_table"
-
-
-# def test_select_with_where():
-#     query = select().from_(TestTable).where(TestTable.age > 18).build()
-#     assert query == "SELECT * FROM test_table WHERE age > 18"
-
-
-# def test_select_with_order_by():
-#     query = select().from_(TestTable).order_by(TestTable.name.asc()).build()
-#     assert query == "SELECT * FROM test_table ORDER BY name ASC"
-
-
-# def test_select_with_limit():
-#     query = select().from_(TestTable).limit(10).build()
-#     assert query == "SELECT * FROM test_table LIMIT 10"
-
-
-# def test_select_with_multiple_conditions(user: User):
-#     query = (
-#         select()
-#         .from_(TestTable)
-#         .where(lambda c: (c.self.age > 18) & (c.self.name.like("A%")))
-#         .order_by(lambda c: c.self.age.desc)
-#         .limit(5)
-#         .build()
-#     )
-#     assert (
-#         query
-#         == "SELECT name, age FROM test_table WHERE age > 18 AND name LIKE 'A%' ORDER BY age DESC LIMIT 5"
-#     )
